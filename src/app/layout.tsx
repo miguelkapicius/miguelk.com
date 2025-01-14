874cfcc8c32abffe60d419974bc696990696c119
Miguel Kapicius <miguelkapicius@gmail.com>
2025-01-14 01:04:48 -0300

    feat: adicionei o google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header/header";
 import { Footer } from "@/components/footer/footer";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const poppins = Poppins({
     weight: ["300", "500", "700"],
@@ -29,6 +30,7 @@ export default function RootLayout({
                 <main className="flex-1 flex pb-12 mt-8">{children}</main>
                 <Footer />
             </body>
+            <GoogleAnalytics gaId="G-T0VTW5S9DW" />
         </html>
     );
 }
